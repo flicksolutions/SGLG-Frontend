9d79674e7bc81201bb52b980fd4d8d6160879d2f
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-03-06 12:15:52 +0100

    get home-items

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,53 +1,61 @@
 <script>
 	import { _ } from 'svelte-i18n';
+	import DirectusSDK from '@directus/sdk-js';
+	import Sticker from '../components/Sticker.svelte';
+	import { onMount } from 'svelte';
+	import { locale } from 'svelte-i18n';
+
+	const directus = new DirectusSDK('https://backend.ruralhistory.ch');
+
+	let directories;
+	let items = [];
+
+
+	onMount(async () => {
+		directories = (await directus.items('directories').read()).data.directories; //get the collections that are relevant as a directory
+		for (const directory of directories) {
+			items = [...items, ...(await directus.items(directory).read({ //get each item in every relevant collection
+				fields: ['title','id','date'] //get only these fields
+			})).data.map(i => {
+				i.collection = directory //add the collection to the item
+				if ($locale.slice(0,2) !== 'de') {
+					directus.items(`${directory}_translations`).read({ //get the translated title and set it as the new title
+						filter: {
+							[`${directory}_id`]: i.id,
+							languages_code: $locale.slice(0,2)
+						},
+						fields: 'title'
+					}).then(r => i.title = r.data[0].title)
+				}
+
+				return i
+			})];
+		}
+	})
 </script>
 
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>{$_('Schweizerische Gesellschaft für ländliche Geschichte')}</title>
 </svelte:head>
 
-<h1>Great success!{$_('page_title')}</h1>
-
-<figure>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><i>this should be red</i><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div><!-- this element needs the background-img -->
+	<Sticker />
+	<section>
+		<h2>Aktuell</h2>
+		<ol>
+			{#each items as item}
+			<li>
+				<a href="{`/directory/detail/${item.collection}/${item.id}`}" class="{item.collection}">{item.title}</a>
+				<div><p>{item.date}</p><p>{item.collection}</p></div>
+			</li>
+			{/each}
+		</ol>
+		<a class="button" href="/directory">Alle Einträge</a>
+	</section>
+	<!-- other content -->
+</div>
 
 <style lang="scss">
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-		i {
-			color: red;
-		}
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
