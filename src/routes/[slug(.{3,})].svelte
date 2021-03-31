4c5590a675b007de6b8ca807f019b80ae03ebed5
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-03-31 22:48:45 +0200

    regex routing

---
+++ src/routes/[slug(.{3,})].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+    export async function preload({ params }) {
+        console.log(params.slug)
+        const { slug } = params;
+        return { slug }
+    }
+</script>
+
+<script>
+    import {onMount} from "svelte";
+    import { goto } from '@sapper/app';
+    import { locales, locale } from 'svelte-i18n';
+
+    export let slug
+
+    onMount(async () => {
+        const browserLang = navigator.language.substring(0,2)
+        const lang = $locales.includes(browserLang) ? browserLang : 'de';
+        $locale = lang;
+        goto(`/${lang}/${slug}`, {replaceState: true});
+    });
+</script>
+
+redirecting...
