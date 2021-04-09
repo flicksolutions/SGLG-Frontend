52c5de2a5ad5146cebcf77347dc09a34aaad08dc
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-04-09 15:21:03 +0200

    set title and links for cfp & review

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const SVGS = {
     "event": '/svg/Icon-Veranstaltungen.svg',
     //"publications": '/svg/Icon',
-    "cfp": '/svg/Icon-Call-for-papers',
-    "review": '/svg/Icon-Rezensionen-Berichte'
+    "call_for_paper": '/svg/Icon-Call-for-papers.svg',
+    "review": '/svg/Icon-Rezensionen-Berichte.svg'
 }
--- src/routes/[lang([a-z]{2})]/index.svelte
+++ src/routes/[lang([a-z]{2})]/index.svelte
@@ -12,7 +12,7 @@
     let windowWidth;
     let featuredImg;
 
-    let fields = ['content','items.item.*','items.collection'];
+    let fields = ['content','items.item.*','items.collection', 'items.item.event.*', 'items.item.event_publication.item.*', 'items.item.event_publication.collection'];
     $locale !== 'de' && fields.push('items.item.translations.*'); // if we are not in default locale, we need to get the translations of the items
     getItems({
         locale: $locale.slice(0, 2),
@@ -20,15 +20,31 @@
         translatedFields: ['content'],
         collections: ['homepage']
     }).then(i => {
-        items = i[0].items.map(i => { //we set the items to display on the page. For that we consume the items from the response.
-            return {                  //it may look funny to fiddle around with the items. but the reason is translation. I do this here because I want my html nice and understandable.
+        items = i[0].items.map(i => {//we set the items to display on the page. For that we consume the items from the response.
+            let cleanItem = {                  //it may look funny to fiddle around with the items. but the reason is translation. I do this here because I want my html nice and understandable.
                 ...i,
                 item: {
                     ...i.item,
                     ...i.item.translations?.[0],  //but also replace any translatable fields with the translated ones.
                     id: i.item.id                 //exept for the id
                 }
+            };
+            switch (i.collection) {
+                case "call_for_paper":
+                    cleanItem.item.title = $_('cfp_title', { values: {title:i.item.event.title}});
+                    cleanItem.link = `/directory/detail/event/${i.item.event.id}`;
+                    cleanItem.item.internal = i.item.event.internal;
+                    break;
+                case "review":
+                    cleanItem.item.title = $_('review_title', { values: {title: i.item.event_publication[0].item.title}});
+                    cleanItem.link = `/directory/detail/${i.item.event_publication[0].collection}/${i.item.event_publication[0].item.id}`;
+                    cleanItem.item.internal = i.item.event_publication[0].item.internal;
+                    break;
+                default:
+                    cleanItem.link = `/directory/detail/${i.collection}/${i.item.id}`
             }
+
+            return cleanItem
         });
         content = i[0].content;
     });
@@ -60,7 +76,7 @@
     <ol>
         {#each items as item}
             <li class:internal={item.item.internal}>
-                <a href="{`/directory/detail/${item.collection}/${item.item.id}`}" class="{item.collection}">
+                <a href="{item.link}" class="{item.collection}">
                     {#if SVGS[item.collection]}<InlineSVG src={SVGS[item.collection]} class="svg"/>{/if}
                     <h2>{item.item.title}</h2>
                 </a>
@@ -68,7 +84,7 @@
                     {#if item.item.date}
                     <p>{$date(new Date(item.item.date), { month: 'numeric', day: 'numeric', year: 'numeric' })}</p>
                     {/if}
-                    <p>{item.collection}</p>
+                    <p>{item.collection.replaceAll('_', ' ')}</p>
                 </div>
             </li>
         {/each}
