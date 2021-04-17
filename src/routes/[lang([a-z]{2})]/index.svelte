f7c79dc7dce0ceef837741b957d4ffab96cfe457
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-04-17 13:42:46 +0200

    homepage with new db structure

--- src/routes/[lang([a-z]{2})]/index.svelte
+++ src/routes/[lang([a-z]{2})]/index.svelte
@@ -2,7 +2,7 @@
     import { locale, _, date } from 'svelte-i18n';
     import { onMount } from 'svelte';
     import Sticker from '../../components/Sticker.svelte';
-    import { getItems, setBg, getBg } from '../../functions';
+    import { getItems, setBg, getBg, directus } from '../../functions';
     import { SVGS } from '../../constants';
     import InlineSVG from 'svelte-inline-svg';
     import { truncate } from 'htmlsave';
@@ -13,41 +13,42 @@
     let windowWidth;
     let featuredImg;
 
-    let fields = ['content','items.item.*','items.collection', 'items.item.event.*', 'items.item.event_publication.item.*', 'items.item.event_publication.collection'];
-    $locale !== 'de' && fields.push('items.item.translations.*'); // if we are not in default locale, we need to get the translations of the items
-    getItems({
-        locale: $locale.slice(0, 2),
-        fields: fields,
-        translatedFields: ['content'],
-        collections: [{directory: 'homepage'}]
-    }).then(i => {
-        items = i[0].items.map(i => {//we set the items to display on the page. For that we consume the items from the response.
+    //get the content
+    let fields = ['content', 'items.references.entities_related_id.title', 'items.references.entities_related_id.internal', 'items.itemtype.directory', 'items.content', 'items.title', 'items.date', 'items.event_type', 'items.publication_type'];
+    let deep = {
+        "items": {
+            "_filter": {
+                "status": {
+                    "_eq": "published"
+                }
+            }
+        }
+    };
+    if ($locale !== 'de') { // if we are not in default locale, we need to get the translations of the items
+        fields.push('translations.content', 'items.translations.title', 'items.translations.content');
+        const trans = {"_filter": {
+                'languages_code': {
+                    "_eq": $locale
+                }
+            }
+        }
+        deep.items.translations = trans;
+        deep.translations = trans;
+    }
+    directus.singleton('homepage').read({ fields, deep }).then(json => {
+        items = json.items.map(i => {//we set the items to display on the page. For that we consume the items from the response.
             let cleanItem = {                  //it may look funny to fiddle around with the items. but the reason is translation. I do this here because I want my html nice and understandable.
                 ...i,
-                item: {
-                    ...i.item,
-                    ...i.item.translations?.[0],  //but also replace any translatable fields with the translated ones.
-                    id: i.item.id                 //exept for the id
-                }
+                ...i.translations?.[0],  //but also replace any translatable fields with the translated ones.
+                id: i.id                 //exept for the id
             };
-            switch (i.collection) {
-                case "call_for_paper":
-                    cleanItem.item.title = $_('cfp_title', { values: {title:i.item.event.title}});
-                    cleanItem.link = `${$locale}/directories/detail/event/${i.item.event.id}`;
-                    cleanItem.item.internal = i.item.event.internal;
-                    break;
-                case "review":
-                    cleanItem.item.title = $_('review_title', { values: {title: i.item.event_publication[0].item.title}});
-                    cleanItem.link = `${$locale}/directories/detail/${i.item.event_publication[0].collection}/${i.item.event_publication[0].item.id}`;
-                    cleanItem.item.internal = i.item.event_publication[0].item.internal;
-                    break;
-                default:
-                    cleanItem.link = `${$locale}/directories/detail/${i.collection}/${i.item.id}`
+            if (i.references[0]?.entities_related_id?.title) {
+                cleanItem.title = $_(`${i.itemtype.directory}_title`, {values: {title: i.references[0].entities_related_id.title}});
+                cleanItem.internal = i.references[0].entities_related_id.internal;
             }
-
             return cleanItem
         });
-        content = i[0].content;
+        content = json.content;
     });
 
     onMount(async () => {
@@ -76,24 +77,24 @@
 
     <ol>
         {#each items as item}
-            <li class:internal={item.item.internal}>
-                <a href="{item.link}" class="{item.collection}">
+            <li class:internal={item.internal}>
+                <a href="{`${$locale}/directories/detail//${item.id}`}" class="{item.collection}">
                     {#if SVGS[item.collection]}<InlineSVG src={SVGS[item.collection]} class="svg"/>{/if}
-                    <h2>{item.item.title}</h2>
+                    <h2>{item.title}</h2>
                 </a>
                 <div class="meta">
-                    {#if item.item.date}
-                    <p>{$date(new Date(item.item.date), { month: 'numeric', day: 'numeric', year: 'numeric' })}</p>
+                    {#if item.date}
+                    <p>{$date(new Date(item.date), { month: 'numeric', day: 'numeric', year: 'numeric' })}</p>
                     {/if}
-                    <p>{item.collection.replaceAll('_', ' ')}</p>
+                    <p>{item.itemtype.directory.replaceAll('_', ' ')}</p>
                 </div>
                 <div class="content">
-                {#if item.item.content}{@html truncate(item.item.content, 250)}{/if}
+                {#if item.content}{@html truncate(item.content, 250)}{/if}
                 </div>
             </li>
         {/each}
     </ol>
-    <a class="button" href="/directory" >Alle Einträge</a>
+    <a class="button" href={`${$locale}/directories`} >Alle Einträge</a>
 </section>
 {#each content as block}
 <section class:colorful={block.style} class="content-block">
