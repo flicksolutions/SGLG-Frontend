e08c81c76b6a9c4ce6a45226426f2c8fc80a5a30
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-04-22 18:20:30 +0200

    formstyling

--- src/routes/[lang([a-z]{2})]/directories/index.svelte
+++ src/routes/[lang([a-z]{2})]/directories/index.svelte
@@ -10,16 +10,10 @@
     import { locale, _, date } from 'svelte-i18n';
     import {onMount} from "svelte";
 
-    //let directories = [];
     export let directoryObjects;
     let directories = directoryObjects.map(d => d.directory)
     let table = [];
     let columns = [];
-    /*directoryPromise.then(i => {
-        directoryObjects = i;
-        directories = i.map(d => d.directory);
-        selectors.categories = directories;
-    })*/
     const selectors = {
         categories: directories,
         onlySglg: false,
@@ -33,10 +27,6 @@
     let results;
 
     let getResults = async ({ categories = [], onlySglg = false, dateFrom = "", dateTo = "", query = "", page = 1, sort = "" }) => {
-        /*let fields = ['id', 'itemtype', 'internal', 'title', 'date', 'event_type', 'event_hosted_by', 'event_end_date',
-            'event_place', 'publications_publisher', 'publications_series', 'link', 'files',
-            'publications_associated_instititutions', 'publications_person', 'content', 'publications_released_in',
-            'publications_type', 'references', 'referenced_by'];*/
         let returnColumns, returnTable;
         let fields = [...new Set(categories.flatMap(c => directoryObjects.find(o => o.directory === c).frontend_fields)), 'itemtype.directory', 'id', 'references.entities_related_id.title'];
         let deep = {};
@@ -62,7 +52,6 @@
         }
 
         if ($locale !== 'de') {
-            //fields.push('translations.title', 'translations.event_hosted_by', 'translations.link', 'translations.content');
             fields.push('translations.*');
             deep.translations = {"_filter": {
                     'languages_code': {
@@ -90,7 +79,6 @@
             }
             row.itemtype = row.itemtype.directory;
             for (let col in row) {
-                //if ((typeof row[col] === 'string' && !row[col]) || (Array.isArray(row[col]) && !row[col].length)) {
                 if (!row[col]) {
                     if (!emptyCols[col]) {
                         emptyCols[col] = 1;
@@ -127,53 +115,96 @@
         setResults();
     }
 
+    const hideElement = (e) => e.target.style.display = "none";
+
+    const dateLabels = [];
+
     onMount(() => setResults());
 </script>
-<h1>Verzeichnisse</h1>
-<div class="directory-nav">
+
+<section class="filters">
+    <h1>Verzeichnisse</h1>
     <div class="category-selectors">
-        <label>
-            <input type="checkbox" id="all"
+        <label><input type="checkbox" id="all"
                    checked={selectors.categories.length === directories.length}
                    on:click={() => {selectors.categories.length === directories.length ? selectors.categories = [] : selectors.categories = directories}}>
             {$_('all')}</label>
         {#each directories as category}
             <label><input type="checkbox" bind:group={selectors.categories} value={category}>{category}</label>
         {/each}
+
     </div>
-    <section class="filters">
-        <label><input type="checkbox" bind:checked={selectors.onlySglg}>{$_('onlySglg')}</label>
-        <label><input type="date" bind:value={selectors.dateFrom}>{$_('from')}</label>
-        <label><input type="date" bind:value={selectors.dateTo}>{$_('to')}</label>
-        <label><input type="search" bind:value={selectors.query}>{$_('query')}</label>
-        <button on:click={setResults}>{$_('search')}</button>
-    </section>
-    <section class="table">
-        <table>
+    <label><input type="checkbox" bind:checked={selectors.onlySglg}>{$_('onlySglg')}</label>
+    <div class="date-selectors">
+        <label for="date" on:click={hideElement} bind:this={dateLabels[0]}>{$_('from')}</label>
+        <input id="date" type="date" bind:value={selectors.dateFrom} on:focus={() => dateLabels[0].style.display = "none"}>
+        <label for="end-date" on:click={hideElement} bind:this={dateLabels[1]}>{$_('to')}</label>
+        <input id="end-date" type="date" bind:value={selectors.dateTo} on:focus={() => dateLabels[1].style.display = "none"}>
+    </div>
+    <label class="search">{$_('query')}<input type="search" bind:value={selectors.query}></label>
+    <button on:click={setResults}>{$_('search')}</button>
+</section>
+<section class="table">
+    <table>
+        <tr>
+            {#each columns as col}
+                <th on:click={() => sortResults(col)}>{col}</th>
+            {/each}
+        </tr>
+        {#each table as row (row.id)}
             <tr>
                 {#each columns as col}
-                    <th on:click={() => sortResults(col)}>{col}</th>
+                    <td>{#if col === 'title'}
+                        <a href={`${$locale}/directories/detail/${row.id}`}>{row[col] ?? $_(`${row.itemtype}_title`, {values: {title: row.references?.[0].entities_related_id.title}})}</a>
+                        {:else if (typeof row[col] === 'string' && row[col]) || (Array.isArray(row[col]) && row[col].length) }
+                            {#if typeof row[col] !== "string"}
+                                {row[col].length}
+                            {:else}
+                                {@html row[col]}
+                            {/if}
+                    {/if}</td>
                 {/each}
             </tr>
-            {#each table as row (row.id)}
-                <tr>
-                    {#each columns as col}
-                        <td>{#if col === 'title'}
-                            <a href={`${$locale}/directories/detail/${row.id}`}>{row[col] ?? $_(`${row.itemtype}_title`, {values: {title: row.references?.[0].entities_related_id.title}})}</a>
-                            {:else if (typeof row[col] === 'string' && row[col]) || (Array.isArray(row[col]) && row[col].length) }
-                                {#if typeof row[col] !== "string"}
-                                    {row[col].length}
-                                {:else}
-                                    {@html row[col]}
-                                {/if}
-                        {/if}</td>
-                    {/each}
-                </tr>
-            {/each}
-            <tr></tr>
-        </table>
-        {#if table.length}
-            <button on:click={loadMore}>{$_('load more...')}</button>
-        {/if}
-    </section>
-</div>
+        {/each}
+        <tr></tr>
+    </table>
+    {#if table.length}
+        <button on:click={loadMore}>{$_('load more...')}</button>
+    {/if}
+</section>
+
+<style lang="scss">
+  @import "../../../style/theme.scss";
+  .category-selectors {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+  .date-selectors {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    label {
+      background-color: white;
+      grid-row: 1;
+      z-index: 2;
+    }
+    input {
+      grid-row: 1;
+    }
+    label:first-of-type, input:first-of-type {
+      grid-column: 1;
+    }
+    label:last-of-type, input:last-of-type {
+      grid-column: 2;
+    }
+  }
+  .search {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
+  .filters {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 2em;
+  }
+</style>
