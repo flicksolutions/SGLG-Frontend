6a183ce29df1638dc0c81b70de8c5d99823b4a4f
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-04-26 15:23:20 +0200

    add (working) detail page

---
+++ src/routes/[lang([a-z]{2})]/directories/detail/[id].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+    import { directus } from '../../../../functions';
+    export async function preload({ params }) {
+        try {
+            const item = await directus.items('entities').readOne(params.id);
+            return { item };
+        } catch (err) {
+            this.error(403, 'No Permission');
+        }
+    }
+</script>
+<script>
+    import { directories } from '../../../../constants';
+    export let item;
+    export let err = false;
+</script>
+
+<button>back</button>
+{JSON.stringify(item)}
