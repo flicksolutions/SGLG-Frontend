a5beae3ed2777fc6dad85c4e1c3f1b7187e15136
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-05-03 16:06:06 +0200

    show content

--- src/components/ContentBoxes.svelte
+++ src/components/ContentBoxes.svelte
@@ -1,5 +1,44 @@
 <script>
-    export let content;
+    import { _ } from 'svelte-i18n';
+
+    export let content = {};
 </script>
 
-{JSON.stringify(content)}
+{#each Object.entries(content) as [title, val]}
+    <div class="top-line">
+    {#if !Array.isArray(val) && val}
+        <h3>{$_(title)}</h3>
+        {#if title.includes('date')}
+            <p>&#8594; {val}</p>
+        {:else if title.includes('content')}
+            {@html val}
+        {:else if typeof val === 'string'}
+            <p>{val}</p>
+        {:else}
+            <p>{JSON.stringify(val)}</p>
+        {/if}
+    {:else if Array.isArray(val) && val[0]}
+        <h3>{$_(title)}</h3>
+        {#if title.includes('link')}
+            {#each val as arVal}
+                {@html arVal}
+            {/each}
+        {:else}
+            {#each val as arVal}
+                <p>{arVal}</p>
+            {/each}
+        {/if}
+    {/if}
+    </div>
+{/each}
+
+
+<style lang="scss">
+  @import "../../../style/theme.scss";
+    * {
+      color: $dark-green;
+    }
+  .top-line {
+    border-top: solid 1px $line-grey;
+  }
+</style>
--- src/routes/[lang([a-z]{2})]/directories/detail/[id].svelte
+++ src/routes/[lang([a-z]{2})]/directories/detail/[id].svelte
@@ -2,10 +2,17 @@
     import { directus } from '../../../../functions';
     export async function preload({ params }) {
         try {
-            let item = await directus.items('entities').readOne(params.id, {fields: ['*', 'itemtype.*', 'references.entities_related_id']});
+            let item = await directus.items('entities').readOne(params.id,
+                {fields: ['*', 'itemtype.*', 'references.entities_related_id', 'referenced_by.entities_id.*', 'referenced_by.entities_id.itemtype.*']}
+            );
             if (!item.title) {
                 console.log('theres no title! returning the referenced object instead.')
-                item = await directus.items('entities').readOne(item.references?.[0].entities_related_id, {fields: ['*', 'itemtype.*']});
+                item = await directus.items('entities').readOne(item.references?.[0].entities_related_id,
+                    {fields: ['*', 'itemtype.*']}
+                );
+            }
+            if (item.link[0]) {
+                item.link = item.link.map(object => `<a href="${object.url}">${object.url}</a>`)
             }
             return { item };
         } catch (err) {
@@ -17,14 +24,32 @@
     import { SVGS } from '../../../../constants';
     import InlineSVG from 'svelte-inline-svg';
     import ContentBoxes from '../../../../components/ContentBoxes.svelte';
+    import { _, date } from 'svelte-i18n';
 
     export let item;
-    const frontEndProps = Object.keys(item)
-        .filter(key => item.itemtype.frontend_fields.includes(key))
-        .reduce((obj, key) => {
-        obj[key] = item[key];
-        return obj
-    },{});
+
+    const cleanProps = dirtyItem => {
+        const filterKeys = ['internal', 'referenced_by', 'references']
+        return Object.keys(dirtyItem)
+            .filter(key => dirtyItem.itemtype.frontend_fields.includes(key) && !filterKeys.includes(key))
+            .reduce((obj, key) => {
+                //convert val to date
+                if (key.includes('date')){
+                    obj[key] = $date(new Date(dirtyItem[key]),
+                        dirtyItem.itemtype.directory === 'publications' ?
+                            {year: 'numeric'} :
+                            { month: 'numeric', day: 'numeric', year: 'numeric' }
+                    );
+                } else {
+                    obj[key] = dirtyItem[key];
+                }
+
+                return obj;
+            },{})
+    }
+
+    const frontEndProps = cleanProps(item);
+    const references = item?.referenced_by.map(ref => cleanProps(ref.entities_id));
     let windowWidth;
 </script>
 <svelte:window bind:innerWidth={windowWidth} />
@@ -35,7 +60,11 @@
 <section>
 <h1 class:internal={item.internal}><InlineSVG src={SVGS[item.itemtype.directory]} class="svg"/> {item.title}</h1>
     <ContentBoxes content={frontEndProps}/>
-    <button on:click={() => window.history.back()}>back</button>
+    {#each item?.referenced_by as ref,i}
+        <h3>{$_(ref.entities_id.itemtype.directory)}:</h3>
+        <ContentBoxes content={references[i]}/>
+    {/each}
+    <button class="button" on:click={() => window.history.back()}>back</button>
 </section>
 
 
