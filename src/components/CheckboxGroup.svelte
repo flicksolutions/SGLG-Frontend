2f74bd91611684246bcf66d564b502c672fde569
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-05-07 19:18:29 +0200

    mobile styling of content pages

--- src/components/CheckboxGroup.svelte
+++ src/components/CheckboxGroup.svelte
@@ -13,39 +13,3 @@
         <InlineSVG src={'/svg/x.svg'} class="chk-icon"/>
         <InlineSVG src={SVGS[checkbox.value]} class="svg"/>{checkbox.label}</label>
 {/each}
-
-<style lang="scss">
-  @import "../style/theme.scss";
-  input[type=checkbox] {
-    display: none;
-    & + label {
-      :global(.chk-icon) {
-        display: none;
-      }
-      :global(.svg) {
-        margin: 0 0.6em;
-        width: 22px;
-        display: block;
-        float: left;
-        fill: $dark-green;
-      }
-      &:before {
-        content: "";
-        border: 1px solid $line-grey;
-        width: 14px;
-        height: 14px;
-        display: block;
-        float: left;
-        margin: 4px 0 0 0;
-        //margin-right: 5px;
-      }
-    }
-    &:checked + label :global(.chk-icon) {
-      display: block;
-      position: absolute;
-      margin: 7px 0 0 1.5px;
-      stroke-width: 3;
-      width: 13px;
-    }
-  }
-</style>
