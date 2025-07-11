32670de23e4631172e7ad7def8ef59e1689061da
flicksolutions <sebastian@flick.solutions>
2025-07-11 17:39:39 +0200

    add action

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import { sveltePreprocess } from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
 	preprocess: sveltePreprocess({
 		scss: {
