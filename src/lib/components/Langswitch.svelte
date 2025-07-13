8e0a736ddabf54d1a9f777071428ad3fd0bfff11
flicksolutions <sebastian@flick.solutions>
2025-07-13 17:33:10 +0200

    undo some of the changes

--- src/lib/components/Langswitch.svelte
+++ src/lib/components/Langswitch.svelte
@@ -19,7 +19,7 @@
 	{#each locales as lang}
 		<li>
 			<a
-				href={linkHandler(`/${page.url.pathname.split('/').filter(Boolean).pop() ?? ''}`, lang)}
+				href={linkHandler(`/${page.url.pathname}`, lang)}
 				class:selected={lang === getLocale()}
 				on:click|preventDefault={() => setLocale(lang)}>{lang.toUpperCase()}</a
 			>
