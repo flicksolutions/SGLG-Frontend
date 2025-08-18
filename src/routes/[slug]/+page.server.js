5989777277d9b0523fcd76586692f0ef6bbff599
flicksolutions <sebastian@flick.solutions>
2025-08-18 22:04:53 +0200

    start with displaying member_lists differently

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -16,7 +16,9 @@ export async function load({ params, fetch, parent }) {
 						'*',
 						'content.page_content_id.*',
 						'content.page_content_id.imagegrid_img.directus_files_id.id',
-						'content.page_content_id.imagegrid_img.directus_files_id.title'
+						'content.page_content_id.imagegrid_img.directus_files_id.title',
+						'lists.memberlists_id.title',
+						'lists.memberlists_id.listfile'
 					],
 					{},
 					getLocale()
@@ -38,14 +40,21 @@ export async function load({ params, fetch, parent }) {
 	};
 	if (content[0]) {
 		const { bgUrl } = await parent();
-		const returnContent = await Promise.all(
-			content[0]?.content.map(async (c) => {
+		const returnContent = await Promise.all([
+			...(content[0]?.content.map(async (c) => {
 				return {
-					...c.page_content_id,
-					member_list: await replaceWithList(c.page_content_id?.member_list)
+					...c.page_content_id
 				};
-			})
-		);
+			}) || []),
+			...(content[0]?.lists.map(async (l) => {
+				console.log(l.memberlists_id);
+				const list = await replaceWithList(l.memberlists_id.listfile);
+				return {
+					title: l.memberlists_id.title,
+					member_list: list || []
+				};
+			}) || [])
+		]);
 		return {
 			meta: { title: content[0]?.title, description: await marked(content[0]?.description) },
 			content: returnContent,
